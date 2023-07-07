20a729e77d5c96130bc16e0c563b0b004aee06d0
Mike Rivnak <rivnakm1@gmail.com>
2023-07-06 21:31:16 -0400

    actual random worldgen

--- src/plugins.rs
+++ src/plugins.rs
@@ -3,6 +3,9 @@ use bevy::prelude::*;
 use crate::components::{Road, RoadType, Tile};
 use crate::worldgen;
 
+const WORLD_X_SIZE: u32 = 20;
+const WORLD_Y_SIZE: u32 = 20;
+const WORLD_Z_SIZE: u32 = 5;
 pub struct WorldGenPlugin;
 
 impl Plugin for WorldGenPlugin {
@@ -13,7 +16,7 @@ impl Plugin for WorldGenPlugin {
 }
 
 fn generate_world(mut commands: Commands) {
-    let tiles = worldgen::generate_world(10, 10, 10)
+    let tiles = worldgen::generate_world(WORLD_X_SIZE, WORLD_Y_SIZE, WORLD_Z_SIZE)
         .into_iter()
         .map(|vec| Tile { position: vec });
     commands.spawn_batch(tiles);
--- src/worldgen.rs
+++ src/worldgen.rs
@@ -1,12 +1,14 @@
 use bevy::prelude::*;
+use rand::Rng;
 
 pub fn generate_world(xsize: u32, ysize: u32, zsize: u32) -> Vec<Vec3> {
+    let mut rng = rand::thread_rng();
     let mut world: Vec<Vec3> = Vec::new();
     for x in 0..xsize {
         for y in 0..ysize {
-            for z in 0..zsize {
-                world.push(Vec3 { x: x as f32, y: y as f32, z: z as f32 });
-            }
+            // TODO: use a noise generator rather than random
+            let z: u32 = rng.gen_range(0..zsize);
+            world.push(Vec3 { x: x as f32, y: y as f32, z: z as f32 });
         }
     }
     world
